bf3f241b8b24965218fec62469592d25f4d9d218
Stefano Lacorazza <stefano.lacorazza@gmail.com>
2025-01-10 14:48:04 -0500

    Refactor UserComponent to use computed property for image path and streamline user selection logic

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -14,10 +14,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 export class UserComponent {
   selectedUser = signal(DUMMY_USERS[randomIndex]) ;
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser().avatar
-    
-  }
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
