ac6c33ccd96b43d43b46315d04665ce6014a1b1c
Stefano Lacorazza <stefano.lacorazza@gmail.com>
2025-01-10 14:36:01 -0500

    Enhance UserComponent with dynamic user selection and update template for improved interactivity

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]) ;
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser().avatar
+    
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
 }
