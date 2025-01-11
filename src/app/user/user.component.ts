b9c94f413afdf9f90ca243e83ba92bdacb959d48
Stefano Lacorazza <stefano.lacorazza@gmail.com>
2025-01-11 09:59:36 -0500

    Refactor UserComponent to use input validation for avatar and name properties

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,9 +9,12 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 
 export class UserComponent {
-  @Input({ required: true}) avatar!: string;
-  @Input({ required: true}) name!: string;
+  //@Input({ required: true}) avatar!: string;
+  //@Input({ required: true}) name!: string;
 
+  avatar = input.required<string>();
+  name = input.required<string>();
+  
   get imagePath() {
     return `assets/users/${this.avatar}`;
   }
