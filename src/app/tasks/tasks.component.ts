42047223dff3f613ae8062c5c89066b4031362c7
Stefano Lacorazza <stefano.lacorazza@gmail.com>
2025-01-15 07:40:57 -0500

    Add new task component and integrate task addition functionality

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,6 +13,9 @@ export class TasksComponent {
 
   @Input({required: true}) name!: string;
   @Input({required: true}) userId!: string;
+  @Output() addTask = new EventEmitter<string>();
+
+  isAddingTask = false;
 
   tasks = [
     {
@@ -45,5 +49,10 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+    this.addTask.emit();
   }
 }
